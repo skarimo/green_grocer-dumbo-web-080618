4b3909e0279278fd0da21a3456999bf4275fbd93
Sherzod Karimov <karimovj@gmail.com>
2018-07-25 04:42:39 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -72,9 +72,8 @@ def checkout(cart, coupons)
   
     if items.length == 1 && coupons == 0
       consolidate_cart(cart)
-      apply_clearance(cart)
+        
         cart.each do |k, v|
-          
           total += v[:price]
           
         end 

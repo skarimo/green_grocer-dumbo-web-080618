5bc82fbc26bd0f73a13490f014ea50f5d0e64f3c
Sherzod Karimov <karimovj@gmail.com>
2018-07-25 05:06:04 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -70,14 +70,7 @@ def checkout(items, coupons)
   cart1 = apply_coupons(cart, coupons)
   cart2 = apply_clearance(cart1)
   
-  total = 0
-  
-  cart2.each do |name, price_hash|
-    total += price_hash[:price] * price_hash[:count]
-  end
-  
-  total > 100 ? total * 0.9 : total
-  
+
 end
 
 
